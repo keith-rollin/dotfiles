dda71cd22b0a67f4e3ba2892ef432f34865c3448
Keith Rollin <keith.rollin@gmail.com>
2023-12-08 16:19:28 -0800

    [plugins.lua] Switch from Packer to Lazy.
    Packer.nvim is no longer supported. Its Readme suggests a couple of
    alternatives; I'm switching to lazy.nvim.

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,82 +1,86 @@
--- https://github.com/khuedoan/dotfiles/blob/master/.config/nvim/lua/user/plugins.lua
+-- https://github.com/folke/lazy.nvim
 
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        print("Installing packer close and reopen Neovim...")
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
-end
-
-local packer_bootstrap = ensure_packer()
-
--- TODO: Add an autocmd to call PackerSync if this file is modified?
--- https://www.youtube.com/watch?v=gd_wapDL0V0&
--- https://youtu.be/m62UCkdQ8Ck?t=548
--- https://github.com/khuedoan/dotfiles/blob/master/.config/nvim/lua/user/plugins.lua
-
-local packer_status_ok, packer = pcall(require, "packer")
-local packer_util_status_ok, packer_util = pcall(require, "packer.util")
-if not (packer_status_ok and packer_util_status_ok) then
-    return
-end
-
-packer.init({
-    compile_path = packer_util.join_paths(vim.fn.stdpath("data"), "plugin", "packer_compiled.lua"),
-    display = {
-        open_fn = function()
-            return packer_util.float({ border = "rounded" })
-        end,
-    },
-    git = {
-        clone_timeout = 300, -- Timeout, in seconds, for git clones
-    },
-})
-
--- Plugins get installed to:
+-- TODO: When starting up from scratch, some repositories are successfully
+--       cloned, but then block at some point (during checkout?). Why is this
+--       happening?
+-- TODO: It seems that not everything is properly initialized on a clean
+--       startup. In particular, my colorscheme is not set. Why?
+-- TODO: Investigate the plugins referenced on the lazy.nvim github page.
+--       Re-evaluate the plugins I use below. It's been a while since I added
+--       them to my lists. Are they still the cutting edge, or have they been
+--       superseded?
 --
---      ~/.local/share/nvim/site/pack/packer/start
+-- Clear out the installation by deleting ~/.local/{state,share}/nvim/lazy
 
-return packer.startup(function(use)
-    use("wbthomason/packer.nvim") -- Packer can manage itself.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    use("nvim-lua/plenary.nvim") -- Pre-reqs for a lot of other plugins.
-    use("nvim-lua/popup.nvim")
+plugins = {
+    "nvim-lua/plenary.nvim", -- Pre-reqs for a lot of other plugins.
+    "nvim-lua/popup.nvim",
 
-    use("ConradIrwin/vim-bracketed-paste") -- Handle BPM (bracketed paste mode -- see https://cirw.in/blog/bracketed-paste).
-    use("derekwyatt/vim-fswitch") -- Switching between companion files.
-    use("neovim/nvim-lspconfig") -- Quickstart configs for Nvim LSP
-    use("tpope/vim-commentary") -- Comment/uncomment.
-    use("tpope/vim-sensible") -- Sensible vim defaults.
-    use("vim-autoformat/vim-autoformat") -- Provide easy code formatting in Vim by integrating existing code formatters. NOTE: needs pynvim installed in python.
-    use("vim-scripts/indentpython.vim") -- PEP8 auto-indenting. TODO: This better handles ()'s, []'s, {}'s, and if/else, but doesn't handle long strings. Can I fix those issues?
+    "ConradIrwin/vim-bracketed-paste", -- Handle BPM (bracketed paste mode -- see https://cirw.in/blog/bracketed-paste).
+    "derekwyatt/vim-fswitch", -- Switching between companion files.
+    "neovim/nvim-lspconfig", -- Quickstart configs for Nvim LSP
+    "tpope/vim-commentary", -- Comment/uncomment.
+    "tpope/vim-sensible", -- Sensible vim defaults.
+    "vim-autoformat/vim-autoformat", -- Provide easy code formatting in Vim by integrating existing code formatters. NOTE: needs pynvim installed in python.
+    "vim-scripts/indentpython.vim", -- PEP8 auto-indenting. TODO: This better handles ()'s, []'s, {}'s, and if/else, but doesn't handle long strings. Can I fix those issues?
 
     -- rust-tools
-    use("simrat39/rust-tools.nvim") -- Tools for better development in rust using neovim's builtin lsp
-    use("nvim-telescope/telescope.nvim")
+    "simrat39/rust-tools.nvim", -- Tools for better development in rust using neovim's builtin lsp
+    "nvim-telescope/telescope.nvim",
 
     -- Treesitter
-    use("nvim-treesitter/nvim-treesitter")
-    use("nvim-treesitter/playground")
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/playground",
 
     -- Completion
-    use("hrsh7th/nvim-cmp") -- A completion plugin for neovim coded in Lua.
-    use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words.
-    use("hrsh7th/cmp-cmdline") -- nvim-cmp source for vim's cmdline.
-    use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim builtin LSP client.
-    use("hrsh7th/cmp-nvim-lua") -- nvim-cmp source for nvim lua.
-    use("hrsh7th/cmp-path") -- nvim-cmp source for path.
-    use("hrsh7th/cmp-vsnip") -- nvim-cmp source for vim-vsnip.
-    use("hrsh7th/vim-vsnip") -- Snippet plugin for vim/nvim that supports LSP/VSCode's snippet format.
+    "hrsh7th/nvim-cmp", -- A completion plugin for neovim coded in Lua.
+    "hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words.
+    "hrsh7th/cmp-cmdline", -- nvim-cmp source for vim's cmdline.
+    "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client.
+    "hrsh7th/cmp-nvim-lua", -- nvim-cmp source for nvim lua.
+    "hrsh7th/cmp-path", -- nvim-cmp source for path.
+    "hrsh7th/cmp-vsnip", -- nvim-cmp source for vim-vsnip.
+    "hrsh7th/vim-vsnip", -- Snippet plugin for vim/nvim that supports LSP/VSCode's snippet format.
+}
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
+opts = {
+    lockfile = vim.fn.stdpath("data") .. "/lazy/lazy-lock.json",
+    ui = {
+        icons = {
+            loaded = "•",
+            not_loaded = "o",
+            task = "√ ",
+            list = {
+                "•",
+                ">",
+                "*",
+                "-",
+            },
+        },
+        custom_keys = {
+            ["<localleader>L"] = {
+                function(plugin)
+                    require("lazy.util").float_term({ "lazygit", "log" }, {
+                        cwd = plugin.dir,
+                    })
+                end,
+                desc = "Open lazygit log",
+            },
+        },
+    },
+}
 
-    if packer_bootstrap then
-        require("packer").sync()
-    end
-end)
+require("lazy").setup(plugins, opts)
