a67d418b5a336a55a43d4f069d5cd783c7cca40f
Keith Rollin <keith.rollin@gmail.com>
2023-12-24 23:59:36 -0800

    [plugins.lua] Remove some auto-completion plugins
    I'm finding that the auto-completion mechanism is trying to help me with
    my prose when typing comments. This is annoying, not helpful, getting in
    the way, and frequently wrong. I'm trying to disable this type of
    auto-completion by disabling some completion plugins; I'm not sure it's
    working.

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -56,11 +56,11 @@ local function init_lazy()
 
             -- Completion
             "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-cmdline",
+            -- "hrsh7th/cmp-buffer",
+            -- "hrsh7th/cmp-cmdline",
             "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-path",
+            -- "hrsh7th/cmp-nvim-lua",
+            -- "hrsh7th/cmp-path",
 
             -- Management of LSP, DAP, lint, and formatting tools
             "neovim/nvim-lspconfig",
