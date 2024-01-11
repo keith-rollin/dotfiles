c848a93e553bf4eefff9c2dd05cf785fc1b8fd97
Keith Rollin <keith.rollin@gmail.com>
2024-01-10 23:36:43 -0800

    [misc.lua] Set floating window color to grey

--- vim/lua/user/misc.lua
+++ vim/lua/user/misc.lua
@@ -1,15 +1,13 @@
 -- Color scheme
 
 vim.cmd.highlight("ColorColumn", "ctermbg=DarkRed")
-
+vim.cmd.highlight("NormalFloat", "ctermfg=0", "ctermbg=7") -- Body of floating windows
+vim.cmd.highlight("FloatBorder", "ctermfg=0", "ctermbg=7") -- Border of floating windows
 
 -- Match spec for git merge error markers.
 -- TODO: Is there a lua-native equivalent?
 
 vim.cmd("match ErrorMsg '^(<|=|>){7}([^=].+)?$'")
-
--- Functions/Commands
--- ------------------
 
 -- Reload the init.lua file.
 
