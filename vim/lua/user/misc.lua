5c11af73cee492aec731f329381dda26fcd40462
Keith Rollin <keith.rollin@gmail.com>
2024-01-11 22:53:30 -0800

    [misc.lua] Remove unused funtions
    Remove Reload function. This function worked well when everything was in
    a single vimrc file, but no longer works with the massive suite of lua
    files I now have, especially with all those plugins. It's now much
    simpler to just restart vim.
    
    Remove WhatAmI function. I don't remember exactly why I wanted/needed
    it. But it seems silly to have given that the name of the file I'm
    working on is there in the status line.

--- vim/lua/user/misc.lua
+++ vim/lua/user/misc.lua
@@ -8,11 +8,3 @@ vim.cmd.highlight("FloatBorder", "ctermfg=0", "ctermbg=7") -- Border of floating
 -- TODO: Is there a lua-native equivalent?
 
 vim.cmd("match ErrorMsg '^(<|=|>){7}([^=].+)?$'")
-
--- Reload the init.lua file.
-
-vim.api.nvim_create_user_command("Reload", ":source " .. vim.env.MYVIMRC, {})
-
--- Remind me what file I'm in.
-
-vim.api.nvim_create_user_command("WhatAmI", ':echo expand("%")', {})
