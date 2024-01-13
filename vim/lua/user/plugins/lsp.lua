7a16db505d0531f9742efd41b325fa0fcea27673
Keith Rollin <keith.rollin@gmail.com>
2024-01-12 22:16:33 -0800

    [lsp.lua] Remove ruff
    The ruff documentation says:
    
    "Drop-in parity with Flake8, isort, and Black
    ...
    "Ruff can be used to replace Flake8 (plus dozens of plugins), Black,
    isort, pydocstyle, pyupgrade, autoflake, and more, all while executing
    tens or hundreds of times faster than any individual tool."
    
    ...yet, I can't seem to get it (well, formatting anyway) to work. So I'm
    removing ruff and explicitly pulling in black and isort.

--- vim/lua/user/plugins/lsp.lua
+++ vim/lua/user/plugins/lsp.lua
@@ -273,7 +273,6 @@ return {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.diagnostics.ruff,
                     null_ls.builtins.formatting.beautysh,
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.isort,
