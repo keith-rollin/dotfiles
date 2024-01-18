90b0ff21900ad9ca26ed5a09bcf53875a848f807
Keith Rollin <keith.rollin@gmail.com>
2024-01-17 20:17:27 -0800

    [indent.lua] Added

---
+++ vim/lua/user/plugins/indent.lua
@@ -0,0 +1,16 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = {
+            -- char = "▏",
+            -- char = "│",
+            -- char = "▕",
+            char = "╎",
+            highlight = { "DiagnosticHint" },
+        },
+        scope = {
+            enabled = false,
+        },
+    },
+}
