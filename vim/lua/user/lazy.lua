703e4e909afb29e54676a81e28dc494f6da844fa
Keith Rollin <keith.rollin@gmail.com>
2024-04-07 14:22:37 -0700

    [lazy.lua] Protect against nvim API change

--- vim/lua/user/lazy.lua
+++ vim/lua/user/lazy.lua
@@ -13,7 +13,7 @@
 -- "lewis6991/gitsigns.nvim",
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
