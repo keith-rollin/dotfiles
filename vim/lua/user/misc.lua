5a4758e6c779b7ad74a14e261fb60ce04aa0689b
Keith Rollin <keith.rollin@gmail.com>
2025-03-18 16:03:44 -0700

    [misc.lua] Use default ColorColumn color by default

--- vim/lua/user/misc.lua
+++ vim/lua/user/misc.lua
@@ -1,7 +1,6 @@
 -- Color scheme
 
 vim.cmd.colorscheme("vim")
-vim.cmd.highlight("ColorColumn", "ctermbg=DarkRed")
 vim.cmd.highlight("NormalFloat", "ctermfg=0", "ctermbg=7") -- Body of floating windows
 vim.cmd.highlight("FloatBorder", "ctermfg=0", "ctermbg=7") -- Border of floating windows
 
