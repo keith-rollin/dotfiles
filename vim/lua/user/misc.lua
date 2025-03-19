f30084458c4dd7cecd40e568f5bbc6e1cb8ed44e
Keith Rollin <keith.rollin@gmail.com>
2025-03-18 19:31:20 -0700

    [misc.lua,options.lua] Better integration with terminal's theme

--- vim/lua/user/misc.lua
+++ vim/lua/user/misc.lua
@@ -1,8 +1,43 @@
 -- Color scheme
 
 vim.cmd.colorscheme("vim")
-vim.cmd.highlight("NormalFloat", "ctermfg=0", "ctermbg=7") -- Body of floating windows
-vim.cmd.highlight("FloatBorder", "ctermfg=0", "ctermbg=7") -- Border of floating windows
+
+-- I want to add "italic" to the current theme's color scheme. I can't just use
+-- the nvim_set_hl() function because it doesn't merge the new attributes--it
+-- completely replaces the existing ones. So I need to get the current highlight
+-- definition, update it, and then set it back.
+--
+-- https://www.reddit.com/r/neovim/comments/wcmqi7/a_simple_utility_function_to_override_and_update/
+-- https://gist.github.com/lkhphuc/ea6db0458268cad1493b2674cb0fda51
+--
+-- Note that the function provided in the gist doesn't work as-is because it
+-- uses the deprecated nvim_get_hl_by_name(). I've updated it to use the newer
+-- nvim_get_hl().
+
+local function update_highlight(hl_name, opts)
+    local is_ok, hl_def = pcall(vim.api.nvim_get_hl, 0, { name = hl_name, create = true })
+    if is_ok then
+        for k, v in pairs(opts) do hl_def[k] = v end
+        vim.api.nvim_set_hl(0, hl_name, hl_def)
+    end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+    group = vim.api.nvim_create_augroup('Color', {}),
+    pattern = "*",
+    callback = function()
+        update_highlight("Comment", { italic = true })
+
+        -- Clear out the old FloatBorder first, or it seems that it will still
+        -- link to WinSeparator -> VertSplit -> Normal -> <cleared>, which seems
+        -- to give us a grey background rather than a black one.
+        vim.api.nvim_set_hl(0, "FloatBorder", {})
+
+        update_highlight("Pmenu", { fg = "Grey", bg = "Black" })
+        update_highlight("FloatBorder", { fg = "Grey", bg = "Black" })
+    end
+})
+
 
 -- Match spec for git merge error markers.
 -- TODO: Is there a lua-native equivalent?
--- vim/lua/user/options.lua
+++ vim/lua/user/options.lua
@@ -1,56 +1,63 @@
 -- Options
 
--- Note:
---  * I would like to enable cursorline, but doing so leave artifacts on the screen
-
-vim.opt.autoindent = true -- Normally 'autoindent' should also be on when using 'smartindent'.
-vim.opt.autowriteall = true -- Write the file when we switch buffers.
+vim.opt.autoindent = true         -- Normally 'autoindent' should also be on when using 'smartindent'.
+vim.opt.autowriteall = true       -- Write the file when we switch buffers.
 vim.opt.clipboard = "unnamedplus" -- Interoperate with the system clipboard.
-vim.opt.completeopt = { "menu", "menuone", "noselect" } -- Mostly for "cmp"
+vim.opt.completeopt = {           -- Mostly for "cmp"
+    "menu",
+    "menuone",
+    "noselect" }
+vim.opt.cursorline = true        -- Highlight the current line.
 vim.opt.grepprg = "rg --vimgrep" -- Use ripgrep for grepping.
-vim.opt.list = true -- Show invisible characters.
+vim.opt.list = true              -- Show invisible characters.
+
 vim.opt.listchars = {
     tab = "▸ ", -- Show TABs as RIGHT-ARROW + SPACEs. (Overrides vim-sensible)
     trail = "•", -- Show trailing spaces as bullets. (Overrides vim-sensible)
     nbsp = "∆", -- Show non-breaking spaces as deltas. (Overrides vim-sensible)
 }
-vim.opt.mouse = "a" -- Use mouse in all (normal, visual, insert, command) modes.
+
+vim.opt.mouse = "a"           -- Use mouse in all (normal, visual, insert, command) modes.
 vim.opt.path:append({ "**" }) -- Recursively search subdirectories for files on tab-complete.
--- vim.opt.scrolloff = 999 -- Keep cursor this many lines from top or bottom for context. (Overrides vim-sensible)
-vim.opt.shiftround = true -- Round indent to multiple of 'shiftwidth'.
-vim.opt.shortmess = "atI" -- Don't show the intro message when starting Vim.
-vim.opt.smartindent = true -- Indent like C-like programs.
-vim.opt.splitbelow = true -- New horizontal splits appear below.
-vim.opt.splitright = true -- New vertical splits appear to the right.
-vim.opt.title = true -- Set window title.
-vim.opt.undofile = true -- Persistent undo
-vim.opt.wrap = false -- Don't wrap text at the right side of the screen.
+vim.opt.shiftround = true     -- Round indent to multiple of 'shiftwidth'.
+vim.opt.shortmess = "atI"     -- Don't show the intro message when starting Vim.
+vim.opt.smartindent = true    -- Indent like C-like programs.
+vim.opt.splitbelow = true     -- New horizontal splits appear below.
+vim.opt.splitright = true     -- New vertical splits appear to the right.
+vim.opt.title = true          -- Set window title.
+vim.opt.undofile = true       -- Persistent undo
+vim.opt.wrap = false          -- Don't wrap text at the right side of the screen.
 
 -- Search/replace related
 
-vim.opt.ignorecase = true -- Ignore case of text and tag searches.
+vim.opt.ignorecase = true    -- Ignore case of text and tag searches.
 vim.opt.inccommand = "split" -- Show prospective changed lines in a split.
-vim.opt.smartcase = true -- Case-insensitive searching unless we type at least one capital letter.
+vim.opt.smartcase = true     -- Case-insensitive searching unless we type at least one capital letter.
 
 -- Line number related.
 
-vim.opt.number = true -- Show absolute line numbers.
+vim.opt.number = true         -- Show absolute line numbers.
 vim.opt.relativenumber = true -- Show relative line numbers; w/ above, accommodates both.
 
 -- TAB related
 
 vim.opt.expandtab = true -- Convert tabs to spaces.
-vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
-vim.opt.softtabstop = 4 -- Number of spaces in tab when editing.
-vim.opt.tabstop = 4 -- Number of visual spaces per TAB.
+vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent.
+vim.opt.softtabstop = 4  -- Number of spaces in tab when editing.
+vim.opt.tabstop = 4      -- Number of visual spaces per TAB.
 
 -- netrw: Configuration
 
-vim.g.netrw_banner = 0 -- Disable banner.
-vim.g.netrw_liststyle = 3 -- Tree view.
+vim.g.netrw_banner = 0       -- Disable banner.
+vim.g.netrw_liststyle = 3    -- Tree view.
 vim.g.netrw_browse_split = 4 -- Open in previous window
 vim.g.netrw_list_hide =
-    "\\(^\\|\\s\\s\\)\\zs\\(\\(.git\\)\\|\\(.DS_Store\\)\\|\\(venv\\)\\|\\(__pycache__\\)\\|\\(.*\\.egg-info\\)\\)"
+"\\(^\\|\\s\\s\\)\\zs\\(\\(.git\\)\\|\\(.DS_Store\\)\\|\\(venv\\)\\|\\(__pycache__\\)\\|\\(.*\\.egg-info\\)\\)"
+
+-- New option as of: Mar 18, 2025
+-- https://github.com/neovim/neovim/commit/62d9fab9af21323e42828748e6761c02020a7aa5
+-- To appear in Neovim 0.11.0
+vim.o.winborder = 'single'
 
 --
 --
